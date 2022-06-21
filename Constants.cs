4803634ef2954537b047e9dab7ce1022d576f8f0
Scipi0s <gamerzach9@gmail.com>
2022-06-21 10:48:55 -0600

    colors

--- Constants.cs
+++ Constants.cs
@@ -27,6 +27,8 @@ namespace Unit05.Game
         public static Color WHITE = new Color(255, 255, 255);
         public static Color YELLOW = new Color(255, 255, 0);
         public static Color GREEN = new Color(0, 255, 0);
+        public static Color ORANGE = new Color(255, 165, 0);
+        public static Color BLUE = new Color(0, 255, 255);
 
     }
 }
